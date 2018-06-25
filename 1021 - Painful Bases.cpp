f12819247e37eec5f0cd5a59508b3d3ad8026427
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2018-06-25 21:23:48 +0600

    1021-1

---
+++ 1021 - Painful Bases.cpp
@@ -0,0 +1,32 @@
+/**
+@author - Rumman BUET CSE'15
+Problem -   1021 - Painful Bases
+Topic- DP
+*/
+
+// LightOJ always needs this format for sure..so I made a copy of it...
+#include <bits/stdc++.h>
+#include<vector>
+#define ll                                      long long int
+#define fi                                      freopen("in.txt", "r", stdin)
+#define fo                                      freopen("out.txt", "w", stdout)
+#define m0(a) memset(a , 0 , sizeof(a))
+#define m1(a) memset(a , -1 , sizeof(a))
+#define inf LLONG_MAX
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define ones(mask)  __builtin_popcount(mask) /// __builtin_popcount : it's a built in function of GCC. Finds out the numbers of 1 in binary representation.
+#define mx 150000
+
+using namespace std;
+
+
+int main()
+{
+
+}
+
+
+
+
+
